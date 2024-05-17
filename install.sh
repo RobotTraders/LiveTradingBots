69c742c5a50d533d989f6c4c7b906145e86d6c7b
Robot Traders <113366890+RobotTraders@users.noreply.github.com>
2024-05-17 11:34:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ cd LiveTradingBots/code
 sudo apt-get install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r ./requirements.txt
+pip install -r ../requirements.txt
 cd ..
